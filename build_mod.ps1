bab6917d4eccd7d084cf765985595a808aad3e60
DIS-Wolv <thomas.lieumont@gmail.com>
2025-05-29 18:39:33 +0200

    update CICD

---
+++ build_mod.ps1
@@ -0,0 +1,45 @@
+
+$ModName = "@DIS-Factions"
+$fileToAdd = @(
+	"dis.jpg",
+	"disMoto.jpg",
+	"dis.paa",
+	"mod.cpp"
+)
+
+# Define paths for the mod and tools
+$KeyFolder = "C:\Users\SteamCMD\Documents\Arma3Keys"
+$A3ToolsPath = "C:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools"
+$AddonBuilderPath = "$A3ToolsPath\AddonBuilder\AddonBuilder.exe"
+
+# check if the mod directory already exists
+if (Test-Path -Path $ModName) {
+	Write-Output "Removing existing mod directory: $ModName"
+	Remove-Item -Path $ModName -Force -Recurse
+}
+
+# create the mod directory structure
+New-Item -Path $ModName -ItemType Directory
+New-Item -Path "$ModName\addons" -ItemType Directory
+New-Item -Path "$ModName\keys" -ItemType Directory
+
+# Copy files to the mod directory
+ForEach ($file in $fileToAdd) {
+	$sourcePath = ".\$file"
+	$destinationPath = "$ModName\$file"
+	
+	Copy-Item -Path $sourcePath -Destination $destinationPath -Force
+	Write-Output "Adding $file to $ModName"
+}
+
+# Process each directory in the addons folder
+$destinationDir = "$(Get-Location)\$ModName\addons\"
+ForEach ($dir in (Get-ChildItem -Path ".\addons" -Directory)) {
+	write-Output "Processing directory: $($dir.name)"
+	$sourcePath = "$(Get-Location)\addons\$($dir.name)"
+	& $AddonBuilderPath $sourcePath $destinationDir -packonly -sign="$KeyFolder\dis.biprivatekey" -toolsDirectory="$A3ToolsPath"
+}
+
+# create the key file
+$pubKeyFilePath = "$KeyFolder\dis.bikey"
+Copy-Item -Path $pubKeyFilePath -Destination "$ModName\keys\dis.bikey" -Force
